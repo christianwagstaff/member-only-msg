4c83ef7508c595c2a370c9551ad41caeefe0871a
Christian <chris23681@gmail.com>
2022-01-09 10:20:53 -0800

    Login and Signup Forms Created

--- controllers/messageController.js
+++ controllers/messageController.js
@@ -1,8 +1,23 @@
 // const async = require("async");
 // const { body, validationResult } = require("express-validator");
 
+// GET Homepage
 exports.index = (req, res) => {
   res.render("index", {
     title: "Message Board",
+  });
+};
+
+// GET Login Screen
+exports.login = (req, res) => {
+  res.render("login", {
+    title: "Log In",
+  });
+};
+
+// GET Signup Screen
+exports.signup = (req, res) => {
+  res.render("signup", {
+    title: "Sign Up",
   });
 };
--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,10 @@ const message_controller = require("../controllers/messageController");
 // Get Message Homepage
 router.get("/", message_controller.index);
 
+// GET LogIn Screen
+router.get("/login", message_controller.login);
+
+// GET Signup Screen
+router.get("/signup", message_controller.signup);
+
 module.exports = router;
